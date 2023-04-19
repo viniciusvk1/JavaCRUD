40fdfd11d3fda618055eba4bb2e747c04395df03
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-19 20:25:51 -0300

    first version of JavaCrud project ready (definitively)

--- src/CrudMain.java
+++ src/CrudMain.java
@@ -21,72 +21,72 @@ public class CrudMain {
         int chooseMenu;
 
         ArrayList<String> products = new ArrayList<>();
+        do {
 
-        // Menu that will appear in the terminal of the chosen IDE
-        System.out.println("=========================================");
-        System.out.println("=====            Menu               =====");
-        System.out.println("=========================================");
-        System.out.println("1 - Add Product:                    =====");
-        System.out.println("2 - Show all products added         =====");
-        System.out.println("3 - Change a product                =====");
-        System.out.println("4 - Delete a product                =====");
-        System.out.println("0 - Close program                   =====");
-        System.out.println("=========================================");
-        System.out.print("\nChoose an option: ");
-        chooseMenu = sc.nextInt();
-        sc.nextLine();
 
-        switch (chooseMenu) {
-            case 1: // Functionality of adding a new product to the list
-                System.out.println("=========================================");
-                System.out.print("Enter the name of the product you want to add: ");
-                String productName = sc.nextLine();
-                products.add(productName);
-                System.out.print("Product added successfully!\n");
-                break;
-            case 2: // Showing all products added
-                System.out.println("=========================================");
-                System.out.println("Available products: ");
-                System.out.println(products);
-                System.out.println();
-                break;
-            case 3: // Modification of added products in the queue
-                System.out.println("=========================================");
-                System.out.print("Choose the product to be changed: ");
-                String searchProductName = sc.nextLine();
+            // Menu that will appear in the terminal of the chosen IDE
+            System.out.println("=========================================");
+            System.out.println("=====            Menu               =====");
+            System.out.println("=========================================");
+            System.out.println("1 - Add Product:                    =====");
+            System.out.println("2 - Show all products added         =====");
+            System.out.println("3 - Change a product                =====");
+            System.out.println("4 - Delete a product                =====");
+            System.out.println("0 - Close program                   =====");
+            System.out.println("=========================================");
+            System.out.print("\nChoose an option: ");
+            chooseMenu = sc.nextInt();
+            sc.nextLine();
 
-                if (products.contains(searchProductName)) { // If the product exists in the list it will be removed and one added in its place
-                    System.out.print("Enter the product change: ");
-                    String changeProductName = sc.nextLine();
-                    products.remove(searchProductName);
-                    products.add(changeProductName);
-                    System.out.println("Altered product!\n");
-                } else { // House does not hold in line nothing will happen
-                    System.out.println("Invalid product!\n");
-                }
-                System.out.println("Updated products: ");
-                System.out.println(products);
-                break;
-            case 4: // Deleting products
-                System.out.println("=========================================");
-                System.out.println("Choose the product you want to delete: ");
-                String removeProduct = sc.nextLine();
-                if (products.contains(removeProduct)) { // if the indicated product exists in the list it will be excluded
-                    products.remove(removeProduct);
-                    System.out.println("Product excluded!\n");
-                } else { // otherwise, nothing will change
-                    System.out.println("Invalid product!\n");
-                }
-                System.out.println("Updated products: ");
-                System.out.println(products);
-                break;
-            default: //Default option to indicate an error of the user's choice
-                System.out.println("Invalid option!\n");
+            switch (chooseMenu) {
+                case 1: // Functionality of adding a new product to the list
+                    System.out.println("=========================================");
+                    System.out.print("Enter the name of the product you want to add: ");
+                    String productName = sc.nextLine();
+                    products.add(productName);
+                    System.out.print("Product added successfully!\n");
+                    break;
+                case 2: // Showing all products added
+                    System.out.println("=========================================");
+                    System.out.println("Available products: ");
+                    System.out.println(products);
+                    System.out.println();
+                    break;
+                case 3: // Modification of added products in the queue
+                    System.out.println("=========================================");
+                    System.out.print("Choose the product to be changed: ");
+                    String searchProductName = sc.nextLine();
 
-                
-        }
-        while (chooseMenu != 0) ;
+                    if (products.contains(searchProductName)) { // If the product exists in the list it will be removed and one added in its place
+                        System.out.print("Enter the product change: ");
+                        String changeProductName = sc.nextLine();
+                        products.remove(searchProductName);
+                        products.add(changeProductName);
+                        System.out.println("Altered product!\n");
+                    } else { // House does not hold in line nothing will happen
+                        System.out.println("Invalid product!\n");
+                    }
+                    System.out.println("Updated products: ");
+                    System.out.println(products);
+                    break;
+                case 4: // Deleting products
+                    System.out.println("=========================================");
+                    System.out.println("Choose the product you want to delete: ");
+                    String removeProduct = sc.nextLine();
+                    if (products.contains(removeProduct)) { // if the indicated product exists in the list it will be excluded
+                        products.remove(removeProduct);
+                        System.out.println("Product excluded!\n");
+                    } else { // otherwise, nothing will change
+                        System.out.println("Invalid product!\n");
+                    }
+                    System.out.println("Updated products: ");
+                    System.out.println(products);
+                    break;
+                default: //Default option to indicate an error of the user's choice
+                    System.out.println("Invalid option!\n");
 
+            }
+        }while (chooseMenu != 0) ;
         sc.close();
     }
 }
